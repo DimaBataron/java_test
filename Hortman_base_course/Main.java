3cb90fa7a74a1a7f7a01c61024b9cbb1d0289aea
Дмитрий <batarond@gmail.com>
2019-08-14 19:36:18 +0300

    2.6
    Модифицирующий метод

--- Hortman_base_course/Main.java
+++ Hortman_base_course/Main.java
@@ -4,11 +4,19 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-       /* myCalendar.printCalendar(2019,9,1);
+       /* myCalendar.printCalendar(2019,9,1); // вывод календаря
         MyMetod dima = new MyMetod();
         int a = dima.modMetod(); // вызов модифицирующего метода возвращающего int
         System.out.println(a);
         */
+       Point coordinates = new Point(3, 4).translate(1, 3).scale((float) 0.5);
+       System.out.println(coordinates.getX() + " " + coordinates.getY());
+
+       // тот-же результат используя модифицирующий метод
+       ModPoint modCoordinates = new ModPoint(3,4);
+       modCoordinates.translate(1,3);
+       modCoordinates.scale(0.5f);
+       System.out.println(modCoordinates.getPx() + " " + modCoordinates.pY);
     }
 
     /*1. Измените представленную в этой главе программу вывода календаря таким образом,
@@ -61,7 +69,85 @@ public class Main {
         }
     }
 
+    /*5. Реализуйте неизменяемый класс P o in t, описывающий точку на плоскости.
+    Предоставьте его конструктор, чтобы задать конкретную точку; конструктор без аргументов,
+    чтобы задать точку в начале координат; а также методы g e tX (), g e tY (),
+    t r a n s la t e () и s c a le ( ) . В частности, метод t r a n s la t e ()
+    должен перемещать точку на определенное расстояние в направлении координат х и у,
+     а метод s c a le () — изменять масштаб по обеим координатам на заданный коэффициент.
+      Реализуйте эти методы таким образом, чтобы они возвращали новые точки в качестве результата.
+       Например, в следующей строке кода:
+    Point р = new Point(3, 4).translate(1, 3).scale(0.5);
+    в переменной р должна быть установлена точка с координатами (2, 3,5).
+    (Тут нет модифицирующих методов. Все методы возвращают новый экземпляр класса типа Point)
+     */
+    private static class Point{
+        private float pX;
+        private float pY;
+        private float pScale;
+        public Point(float pX, float pY){
+            this.pX = pX;
+            this.pY = pY;
+        }
+        public Point(){
+            this(0,0);
+        }
 
+        public float getX(){
+            return pX;
+        }
 
+        public float getY(){
+            return pY;
+        }
+
+        public Point translate(float tX,float tY){
+            tX += pX;
+            tY += pY;
+            return new Point(tX,tY);
+        }
+
+        public Point scale(float scl){
+            float sX = pX*scl;
+            float sY = pY*scl;
+            return new Point(sX,sY);
+        }
+    }
+    /*
+    6.Повторите предыдущее упражнение, но на этот раз сделайте методы
+     t r a n s la t e () и s c a le () модифицирующими.
+     */
+    public static class ModPoint{
+        private float pX;
+        private float pY;
+        private float scl;
+        public ModPoint(float pX,float pY){
+            this.pX= pX;
+            this.pY = pY;
+            scl = 1;
+        }
+        public ModPoint(){
+            this(0,0);
+        }
+
+        public float getPx(){
+            return pX;
+        }
+        public float getPy(){
+            return pY;
+        }
+
+        // Модифицирующий метод перемещения
+        public void translate(float tX,float tY){
+            pX += tX;
+            pY += tY;
+        }
+        // Модифицирующий метод маштабирования
+        public void scale(float scl){
+            this.scl = scl;
+            pX *= scl;
+            pY *= scl;
+        }
+    }
 
 }
