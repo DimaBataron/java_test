e840956c88dc90e80b65533b55575e02bb4e68e3
Дмитрий <batarond@gmail.com>
2019-08-14 17:46:12 +0300

    2.3
    Модифицирующие методы

--- Hortman_base_course/Main.java
+++ Hortman_base_course/Main.java
@@ -4,8 +4,13 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        myCalendar.printCalendar(2019,9,1);
+       /* myCalendar.printCalendar(2019,9,1);
+        MyMetod dima = new MyMetod();
+        int a = dima.modMetod(); // вызов модифицирующего метода возвращающего int
+        System.out.println(a);
+        */
     }
+
     /*1. Измените представленную в этой главе программу вывода календаря таким образом,
  чтобы неделя начиналась с воскресенья. Кроме того, организуйте перевод на новую
   строку в конце предыдущей, но только один раз.
@@ -37,4 +42,26 @@ public class Main {
             }
         }
     }
+    /*3. Может ли модифицирующий метод вообще возвращать что-нибудь,
+    кроме типа void ? Можно ли создать метод доступа с возвращаемым типом void ,
+     т.е. ничего фактически не возвращающий? Приведите по возможности примеры таких методов.
+
+     */
+    //Подкласс доступен только в классе Main
+    private static class MyMetod{ //закрытый вложенный класс
+        private int a;
+        public MyMetod(){
+            a = 15;
+        }
+        // пример модифицирующий метод возвращающий int
+        public int modMetod(){
+            System.out.println("Before:"+ a);
+            a = 20;
+            return a;
+        }
+    }
+
+
+
+
 }
